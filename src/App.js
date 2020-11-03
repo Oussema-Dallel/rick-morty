8ad96e2e52d8467cda6b569b422cd92d0a5f2ec1
Lotfi Messaoudi <volkovmqx@gmail.com>
2020-11-03 23:03:39 +0100

    add modal and infinitescroll

--- src/App.js
+++ src/App.js
@@ -1,49 +1,14 @@
 import "./App.scss";
 import Header from "./components/Header/Header";
 import Container from "./components/Container/Container";
-import Footer from "./components/Footer/Footer";
-import { useState, useEffect } from "react";
-import axios from "axios";
+import { useState } from "react";
 
 function App() {
   const [title] = useState("Rick & Morty");
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    axios({
-      url: "https://rickandmortyapi.com/graphql",
-      method: "post",
-      data: {
-        query: `
-          query {
-            characters (page: 1){
-              results {
-                name
-                image
-                status
-                id
-                gender
-                species
-                episode{
-                  id
-                  name
-                  episode
-                }
-              }
-            }
-          }
-            `,
-      },
-    }).then((result) => {
-      setData(result.data.data.characters.results);
-    });
-  }, []);
-
   return (
     <div className="App">
       <Header title={title} />
-      <Container data={data} />
-      <Footer title={title} />
+      <Container />
     </div>
   );
 }
--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,14 +1,70 @@
 import "./Container.scss";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faInfoCircle, faMars, faVenus, faGenderless} from '@fortawesome/free-solid-svg-icons'
+import { faInfoCircle, faMars, faVenus, faGenderless, faCircleNotch } from '@fortawesome/free-solid-svg-icons'
+import InfiniteScroll from 'react-infinite-scroller';
+import { useState } from "react";
+
+import axios from "axios";
+
 import {
   Card,
   ListGroup,
   ListGroupItem,
-  CardColumns,
+  Col,
+  Row,
+  Container,
+  Modal,
+  Button
 } from "react-bootstrap";
 
-function Container({ data }) {
+function Body() {
+  const [data, setData] = useState([]);
+  const [page, setPage] = useState(1);
+  const [hasMore, setHasMore] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [currentCaracter, setCurrentCaracter] = useState({});
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = (e, id) => {
+    setLoading(true)
+    axios({
+      url: "https://rickandmortyapi.com/graphql",
+      method: "post",
+      data: {
+        query: `
+        query {
+          character(id: ${id}) {
+           name
+           status
+           species
+           type
+           origin {
+             name
+             dimension
+             type
+           }
+           location {
+             name
+             type
+             dimension
+           }
+           episode{
+               name
+               air_date
+               episode
+            }
+         }
+       }
+      `,
+      },
+    }).then((result) => {
+      setCurrentCaracter(result.data.data.character);
+      setShow(true)
+    });
+  };
+  
   const getStatusClassName = (status) => {
     let statusClassName;
     switch (status) {
@@ -39,41 +95,158 @@ function Container({ data }) {
     }
     return genderIcon
   }
+  const loadFunc = () => {
+    axios({
+      url: "https://rickandmortyapi.com/graphql",
+      method: "post",
+      data: {
+        query: `
+          query {
+            characters (page: ${page}){
+              results {
+                name
+                image
+                status
+                id
+                gender
+                species
+                episode{
+                  id
+                  name
+                  episode
+                }
+              }
+            }
+          }
+            `,
+      },
+    }).then((result) => {
+      if(result.data.data.characters.results.length === 0) {
+        setHasMore(false)
+      }
+      setData([...data, ...result.data.data.characters.results]);
+      setPage(page + 1);
+    });
+  }
+
+  
   return (
     <div className="mt-5">
-      <CardColumns>
-        {data.map((d, i) => (
-          <div key={d.id}>
-            <Card style={{ width: "18rem" }}>
-              <div className="card-avatar">
-                <a href="#" className="details"><span><FontAwesomeIcon icon={faInfoCircle} /></span></a>
-                <Card.Img variant="top" src={d.image} />
-              </div>
-              <Card.Body>
-                <Card.Title className={getStatusClassName(d.status)}>
-                  {d.name}
-                </Card.Title>
-              </Card.Body>
-              <ListGroup className="list-group-flush">
-                <ListGroupItem>
-                  <strong>Species: </strong>
-                  {d.species}
-                </ListGroupItem>
-                <ListGroupItem>
-                  <strong>Gender: </strong>
-                  <FontAwesomeIcon icon={getGenderIcon(d.gender)} />
-                </ListGroupItem>
-                <ListGroupItem>
-                  <strong>First seen in: </strong>
-                  {d.episode[0].name} ({d.episode[0].episode})
-                </ListGroupItem>
-              </ListGroup>
-            </Card>
-          </div>
-        ))}
-      </CardColumns>
+      <Container fluid>
+      <InfiniteScroll
+          pageStart={0}
+          loadMore={loadFunc}
+          hasMore={hasMore}
+          loader={<div className="loader" key={0}>Loading ...</div>}
+      >
+       <Row xs={1} md={2} lg={3} xl={4}>
+            {data.map((d, i) => (
+              <Col key={d.id}>
+                <Card style={{ width: "18rem" }}>
+                  <div className="card-avatar">
+                    <Button onClick={(e) => handleShow(e, d.id)} className="details">
+                      { 
+                        loading ? 
+                        <span><FontAwesomeIcon icon={faCircleNotch} spin /></span>
+                        :
+                        <span><FontAwesomeIcon icon={faInfoCircle} /></span>
+                      }
+                    </Button>
+                    <Card.Img variant="top" src={d.image} />
+                  </div>
+                  <Card.Body>
+                    <Card.Title className={getStatusClassName(d.status)}>
+                      {d.name}
+                    </Card.Title>
+                  </Card.Body>
+                  <ListGroup className="list-group-flush">
+                    <ListGroupItem>
+                      <strong>Species: </strong>
+                      {d.species}
+                    </ListGroupItem>
+                    <ListGroupItem>
+                      <strong>Gender: </strong>
+                      <FontAwesomeIcon icon={getGenderIcon(d.gender)} />
+                    </ListGroupItem>
+                    <ListGroupItem>
+                      <strong>First seen in: </strong>
+                      {d.episode[0].name} ({d.episode[0].episode})
+                    </ListGroupItem>
+                  </ListGroup>
+                </Card>
+              </Col>
+            ))}
+           </Row>
+        </InfiniteScroll>
+        </Container>
+        <Modal show={show} onHide={handleClose} onEntered={() => setLoading(false)} dialogClassName="modal-90w">
+          <Modal.Header closeButton>
+            <Modal.Title>{currentCaracter.name}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+               <Container fluid>
+                    <Row>
+                    { currentCaracter.origin ? 
+                      <Col>
+                        <Card
+                          bg="light"
+                        >
+                          <Card.Body>
+                            <Card.Title>Origin</Card.Title>
+                            <Card.Text>
+                                {currentCaracter.origin.name ? currentCaracter.origin.name + " " : ""}
+                                {currentCaracter.origin.type ? "(Type: " + currentCaracter.origin.type  + ") " : ""}
+                            </Card.Text>
+                          </Card.Body>
+                        </Card>
+                      </Col>
+                        : null}
+                      { currentCaracter.location ? 
+                      <Col>
+                        <Card
+                          bg="light"
+                        >
+                          <Card.Body>
+                            <Card.Title>Current Location</Card.Title>
+                            <Card.Text>
+                              {currentCaracter.location.name ? currentCaracter.location.name + " " : ""}
+                              {currentCaracter.location.type ? "(Type: " + currentCaracter.location.type  + ") " : ""}
+                            </Card.Text>
+                          </Card.Body>
+                        </Card>
+                      </Col>
+                        : null}
+                    </Row>
+                    <Row>
+                    { currentCaracter.episode ? 
+                      currentCaracter.episode.map((episode) => (
+                        <Col key={episode.id}>
+                        <Card
+                          bg="light"
+                        >
+                          <Card.Body>
+                            <Card.Text>
+                              {episode.episode ? episode.episode + " " : ""}
+                              {episode.name ? "( " + episode.name + ") " : ""}
+                              {episode.air_date ? episode.air_date : ""}
+                            </Card.Text>
+                          </Card.Body>
+                        </Card>
+                      </Col>
+                      )) : null}
+                      
+                    </Row>
+              </Container>       
+
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
     </div>
   );
 }
 
-export default Container;
+export default Body;
